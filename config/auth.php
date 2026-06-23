7c4e2b31f7b4a4249240dbe2543a1a2f8bbf51f2
coredatacore <166248243+coredatacore@users.noreply.github.com>
2026-06-23 08:30:38 +0800

    Add files via upload

--- config/auth.php
+++ config/auth.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\SystemUser;
+use App\Models\User;
 
 return [
 
@@ -64,7 +64,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', SystemUser::class),
+            'model' => env('AUTH_MODEL', User::class),
         ],
 
         // 'users' => [
